5d96c843a0ad02bb19bb98650c3ba97c0b51e06a
kristian.larsen38@gmail.com <Kristian.larsen38@gmail.com>
2025-01-16 09:01:21 +0100

    fwwfwefwe

--- Legodudes/shopscript.js
+++ Legodudes/shopscript.js
@@ -10,9 +10,22 @@ products.map((product, index) => productHTML +=
             <img src="website_images/PROD_${product.imagefile}" alt="PRODUKTTITTEL"/>
             <a href="#KATEGORISIDE">Ninjago</a>
             <h3>${product.title}</h3>
-            <p>${product.price}</p>
-            <button>Legg i handelkurv</button>
+            <p>Kr. ${product.price}</p>
+            <button onClick="addProductToCart(${product.prodid})">Legg i handelkurv</button>
         </article>`)
 
 // Finn #productList, og fyll den mec verdiene i variabelen productHTML
 document.getElementById("productList").innerHTML = productHTML
+
+// Lage toggle funksjonalitet for handlevogn
+document.getElementById("shoppingcart").addEventListener("click", function() {
+    document.getElementById("cart").classList.toggle("visible")
+})
+
+// Lage addProductToCart
+    function addProductToCart( prodid) {
+        console.log("du vil legge til prodid" + prodid)
+
+        cart.push({ prodid: prodid, quantity: 1 })
+        console.log(cart)
+    }
